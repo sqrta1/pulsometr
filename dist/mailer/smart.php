715da2e7e9dbc923ac1580782f6ffe5bcd26fce8
Korol Alexey <60891357+sqrta1@users.noreply.github.com>
2021-07-13 21:28:02 +0200

    Update smart.php

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'math.sqrta@gmail.com';                 // Наш логин
-$mail->Password = '09110911d';                           // Наш пароль от ящика
+$mail->Username = '***';                 // Наш логин
+$mail->Password = '***';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('math.sqrta@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('fear.343@mail.ru');     // Add a recipient
+$mail->setFrom('***', 'Pulse');   // От кого письмо 
+$mail->addAddress('***');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
